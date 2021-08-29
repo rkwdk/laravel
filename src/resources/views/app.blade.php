1419724101b35220c12902dd8f91ccc33945f0be
zio <rkwdk2020@gmail.com>
2021-08-30 00:04:01 +0900

    vue page 確認

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
-<html lang="ja">
-<head>
-  {{--略--}}
-</head>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<body>
-  <div id="app">
-    @yield('content')
-  </div>
+        <title>Laravel Vue</title>
 
-  <script src="{{ mix('js/app.js') }}"></script>
-  {{--略--}}
-</body>
+    </head>
+    <body>
+
+        <div id="app">
+            <sample-component></sample-component>
+        </div>
+
+    <script src="{{ mix('/js/app.js') }}"></script>
+    </body>
 </html>
