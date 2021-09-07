299cd13edd31a85863723b6d4438b24d34266ac5
zio <rkwdk2020@gmail.com>
2021-09-07 21:22:53 +0900

    add adminLTE

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,43 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
 
-Route::get('/', function () {
-   return view('page.index');
-});
+// Route::get('/', function () {
+//    return view('page.index');
+// });
 Route::get('/login', function () {
     return view('auth.login');
 });
 Route::get('/register', function () {
     return view('auth.register');
 });
-Route::get('/adminlte', function () {
+Route::get('/', function () {
     return view('page.adminlte');
 });
-
-Route::get('/test', function () {
-    return view('page.test',[ 'nums' => rand() ]);
- });
-Route::get('/profile', function () {
-return view('page.profile',[ 'nums' => rand() ]);
+Route::get('/home', function () {
+    return view('page.adminlte');
 });
-
-Route::get('/dashboard', function () {
-return view('page.profile',[ 'name' => "Mr.aaa" ]);
+Route::get('/settings', function () {
+    return view('page.adminlte');
 });
-
-//  Route::get('/test2', function () {
-//      ///TODO 使い道がわからない
-//     return View::first(['page.test','page.second'],[ 'nums' => 0 ]);
-//  });
-
- if (View::exists('page.second')) {
-    Route::get('/test2', function () {
-        return view('page.second')
-        ->with('nums',rand())
-        ->with('name','test');
-     });
-} else {
-    Route::get('/test2', function () {
-        return view('page.error');
-     });
-}
