6fbcfcfe9afd461d91c4c13613bf35b9407896e9
zio <rkwdk2020@gmail.com>
2021-10-09 17:39:26 +0900

    描画範囲を左上と右下の頂点で表現するように修正 座標を文字列で描画するように修正

--- src/resources/js/components/MyCanvas.vue
+++ src/resources/js/components/MyCanvas.vue
@@ -17,36 +17,92 @@ export default {
       type: Number,
       default: 0
     },
+    N: {
+      type: Number,
+      default: 30
+    },
   },
   watch: {
     radius() {
-        this.draw(this.radius,this.width,this.height)
+        this.draw(this.radius,this.width,this.height,this.N)
     }
   },
   methods: {
-    draw(radius,width,height) {
+    draw(radius,width,height,N) {
         this.ctx.clearRect(0, 0, width, height)
+        this.ctx.font = "12px serif";
+
+        let lt = [-0.575,0.55];
+        let ru = [-0.570,0.54];
+        if (ru[0]-lt[0]<0 || ru[1]-lt[1]>0 ){
+            console.log("描画エラー");
+            return
+        }
+
         for(var row=0;row<=height;row+=radius){
             for(var col=0;col<=width;col+=radius){
-                //左上を(-2,-2)、右下を(2,2)の範囲で分割した数刻みで発散するかを計算
+                //左上を(-2,2)、右下を(2,-2)の範囲で分割した数刻みで発散するかを計算
                 // let res = Math.random() > 0.5 ? 255 : 0 ;
-                let res = this.mandelbrot(0,0,col/width*4-2,row/height*4-2,30)
-                if(res == 255){
-                    continue;
+                let res = this.mandelbrot(0,0,
+                    lt[0]+col/width*(ru[0]-lt[0]),
+                    lt[1]+row/height*(ru[1]-lt[1])
+                    ,N
+                )
+                if(res != 255){
+                    this.ctx.beginPath()
+                    this.ctx.arc(col, row, radius, 0, Math.PI * 2);
+                    let rgb =`rgb(${res}, ${res}, ${res})`;
+                    this.ctx.fillStyle = rgb;
+                    this.ctx.fill();
+                    this.ctx.closePath()
                 }
-                let rgb =`rgb(${res}, ${res}, ${res})`;
+            }
+        }
+        for(var row=0;row<=height;row+=radius){
+            for(var col=0;col<=width;col+=radius){
                 this.ctx.beginPath()
-                this.ctx.arc(col, row, radius, 0, Math.PI * 2);
-                this.ctx.fillStyle = rgb;
-                this.ctx.fill();
+                this.ctx.fillStyle = `rgb(255, 0, 0)`;
+                let x = lt[0]+col/width*(ru[0]-lt[0]);
+                let y = lt[1]+row/height*(ru[1]-lt[1]);
+                if ( col == 0 && row == 0 ){
+                    this.ctx.fillText(`lt(${x.toPrecision(2)},${y.toPrecision(2)})`,
+                    col+10, row+10);
+                }
+                if ( col == 0 && row == parseInt(height/2)){
+                    this.ctx.fillText(`l(${x.toPrecision(2)},${y.toPrecision(2)})`,
+                    col+10, row);
+                }
+                if ( col == 0 && row == height){
+                    this.ctx.fillText(`lu(${x.toPrecision(2)},${y.toPrecision(2)})`,
+                    col+10, row-10);
+                }
+                if ( col == parseInt(width/2) && row == 0){
+                    this.ctx.fillText(`t(${x.toPrecision(2)},${y.toPrecision(2)})`,
+                    col, row+10);
+                }
+                if ( col == parseInt(width/2) && row == parseInt(height/2)){
+                    this.ctx.fillText(`O(${x.toPrecision(2)},${y.toPrecision(2)})`,
+                    col, row);
+                }
+                if ( col == parseInt(width/2) && row == height){
+                    this.ctx.fillText(`u(${x.toPrecision(2)},${y.toPrecision(2)})`,
+                    col, row-10);
+                }
+                if ( col == width && row == 0){
+                    this.ctx.fillText(`rt(${x.toPrecision(2)},${y.toPrecision(2)})`,
+                    col-100, row+10);
+                }
+                if ( col == width && row == parseInt(height/2)){
+                    this.ctx.fillText(`r(${x.toPrecision(2)},${y.toPrecision(2)})`,
+                    col-100, row);
+                }
+                if ( col == width && row == height){
+                    this.ctx.fillText(`ru(${x.toPrecision(2)},${y.toPrecision(2)})`,
+                    col-100, row-10);
+                }
                 this.ctx.closePath()
             }
         }
-        this.ctx.beginPath()
-        this.ctx.arc(width/2, height/2, 1, 0, Math.PI * 2);
-        this.ctx.fillStyle = `rgb(255, 0, 0)`;
-        this.ctx.fill();
-        this.ctx.closePath()
     },
     mandelbrot(znr,zni,cr,ci,N){
         if (0>=N){
@@ -64,7 +120,7 @@ export default {
   mounted() {
     this.ctx = this.$el.getContext('2d')
     console.log(`mounted height:${this.height} width:${this.width}`);
-    this.draw(this.radius,this.width,this.height)
+    this.draw(this.radius,this.width,this.height,this.N)
   }
 }
 </script>
