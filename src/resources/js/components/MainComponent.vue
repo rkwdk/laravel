59a2ba44adf9a50b4e9231ba3f8f160f23b59e55
zio <rkwdk2020@gmail.com>
2021-10-10 12:14:28 +0900

    メモ化の途中処理の削除

--- src/resources/js/components/MainComponent.vue
+++ src/resources/js/components/MainComponent.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="example">
-    <MyCanvas :radius="radius" :height="height" :width="width"/>
-    <!-- <p><input type="range" min="0" max="100" v-model.number="radius"></p> -->
+    <MyCanvas :N="N" :left="validateLeft" :top="validateTop" :right="right" :under="under" :height="height" :width="width"/>
+    <p>
+        Left:
+        <button v-on:click="subtract('left')">-</button>
+        <input type="range" min=-2 max=2 :step="step" v-model.number="left">
+        <button v-on:click="add('left')">+</button>
+        {{left.toPrecision(3)}}
+        Top:<input type="range" min=-2 max=2 :step="step" v-model.number="top">{{top.toPrecision(3)}}
+        N:<input type="range" min=10 max=100 step=10 v-model.number="N">{{N.toFixed(3)}}
+    </p>
+    <p>
+        Right:<input type="range" min=-2 max=2 :step="step" v-model.number="right">{{right.toPrecision(3)}}
+        Under:<input type="range" min=-2 max=2 :step="step" v-model.number="under">{{under.toPrecision(3)}}
+    </p>
   </div>
 </template>
 <script>
@@ -12,9 +24,26 @@ export default{
     components: {
         MyCanvas
     },
+    methods:{
+        add(arg1){
+            if (-2 <= this.left && this.left < 2){
+                this.left+=this.step;
+            }
+        },
+        subtract(arg1){
+            if (-2 < this.left && this.left <= 2){
+                this.left-=this.step;
+            }
+        }
+    },
     data() {
         return {
-            radius: 0.5,
+            step:0.01,
+            left:-2,
+            top:2,
+            right:2,
+            under:-2,
+            N:10,
             //ブラウザのサイドメニュとヘッダー以外の高さと幅を取得
             h:document.getElementsByClassName('content-wrapper')[0].offsetHeight,
             w:document.getElementsByClassName('content-wrapper')[0].offsetWidth
@@ -26,80 +55,27 @@ export default{
     mounted : function(){
         console.log('mounted')
     },
-    methods : {
-        //発散値を求めるための再帰関数
-        //znr:Z(n)の実数部、zni:Z(n)の虚数部
-        //cr:cの実数部、ci:cの虚数部、Nは臨界数
-        mandelbrot: function(znr,zni,cr,ci,N){
-            /*
-            Z(n+1)=Z(n)^2+c
-            Z(0)=0+0i
-            cは複素平面上の座標(cr+ci*i)
-            n->∞のとき,Z(n)が発散しないcをマンデルブロ集合という
-            Z(n)=znr+zni*iとして、実部と虚部に分けるとZ(n+1)は
-            zn1r=znr*znr-zni*zni+cr
-            zn1i=2*znr*zni+ci
-            上記を再帰して、途中で発散するかN回目で規定値を超えていた場合255を返す。
-
-            */
-            if (0>=N){
-                let abs = znr*znr+zni*zni;
-                return abs > 255 ? 255 : abs;
-            }
-            let zn1r = (znr * znr) - (zni * zni) + cr;
-            let zn1i = (znr * zni * 2.0) + ci;
-            if (!isFinite(zn1r)||!isFinite(zn1i)){
-                return 255;
-            }
-            return this.mandelbrot(zn1r,zn1i,cr,ci,N-1)
-        },
-    },
     computed: {
-    height: function () {
-        //正方形にしないとマンデルブロ集合が正しく描画されないので、小さい方に合わせる
-        return this.h;// < this.w ? this.h:this.w
-    },
-    width: function () {
-        //正方形にしないとマンデルブロ集合が正しく描画されないので、小さい方に合わせる
-        return this.w;// < this.w ? this.h:this.w
-    },
-    size: function () {
-        //svgのviewportを算出して返す
-        return "0 0 "+this.w+" "+this.h;
-        // return this.h < this.w ?  "0 0 "+this.h*2+" "+this.h*2 : "0 0 "+this.w*2+" "+this.w*2
-    },
-    circles: function () {
-        let circles = [];//svg用の円の配列の格納
-        let count = 0;//svg用の円のid計算用
-        let maxN = 10;//臨界数
-        let r = 1;//塗りつぶし用の半径
-        let h = this.height;//svg用の高さ
-        let w = this.width;//svg用の幅
-        let cr = 0;//グレースケール値の格納
-        let rgb = ``;//svgの描画用の色格納
-        let res = 0;//臨界数を超えたときの値
-
-        //高さを半径で割った数に分割
-        for (var col=0;col<h/r;col+=r){
-            //幅を半径で割った数に分割
-            for (var row=0;row<w/r;row+=r){
-                //左上を(-2,-2)、右下を(2,2)の範囲で分割した数刻みで発散するかを計算
-                res = this.mandelbrot(0,0,row/w*4-2,col/h*4-2,maxN)
-                //結果の値で色を決定(発散したら255)
-                if (res == 0){
-                    rgb = `rgb(255 0 0)`
-                }else {
-                    cr = res % 256;
-                    rgb = `rgb(${cr}, ${cr}, ${cr})`
-                }
-                //描画用の円の生成と配列への格納
-                circles.push({id:count, x:r+row*2*r,y:r+col*2*r,r:r,color:rgb })
-                //円のid
-                count++;
+        validateLeft: function () {
+            if(this.left>this.right){
+                this.right = this.left-this.step;
             }
+            return this.left;
+        },
+        validateTop: function () {
+            if(this.top<this.under){
+                this.under = this.top-this.step;
+            }
+            return this.top;
+        },
+        height: function () {
+            //正方形にしないとマンデルブロ集合が正しく描画されないので、小さい方に合わせる
+            return this.h;// < this.w ? this.h:this.w
+        },
+        width: function () {
+            //正方形にしないとマンデルブロ集合が正しく描画されないので、小さい方に合わせる
+            return this.w;// < this.w ? this.h:this.w
         }
-      return circles;
-    }
   }
 }
 </script>
--- src/resources/js/components/MyCanvas.vue
+++ src/resources/js/components/MyCanvas.vue
@@ -4,10 +4,27 @@
 
 <script>
 export default {
+  data(){
+    return {
+        rad: 1,
+    };
+  },
   props: {
-    radius: {
+    left: {
       type: Number,
-      default: 0
+      default: -1
+    },
+    top: {
+      type: Number,
+      default: 1
+    },
+    right: {
+      type: Number,
+      default: 1
+    },
+    under: {
+      type: Number,
+      default: -1
     },
     height: {
       type: Number,
@@ -19,100 +36,103 @@ export default {
     },
     N: {
       type: Number,
-      default: 30
+      default: 10
     },
   },
+  computed: {
+    inputWatch() {
+      return [this.$props.left, this.$props.top,this.$props.right, this.$props.under, this.$props.N];
+    }
+  },
   watch: {
-    radius() {
-        this.draw(this.radius,this.width,this.height,this.N)
+    inputWatch() {
+        this.draw(this.left,this.top,this.right,this.under,this.width,this.height,this.N)
     }
   },
   methods: {
-    draw(radius,width,height,N) {
+    memoize(recursiveFunction) {
+        var cacheIn = {}
+        return function() {
+            var key = JSON.stringify(arguments);
+            if(cacheIn[key]) {
+                return cacheIn[key];
+            }
+            else {
+                var val = recursiveFunction.apply(this, arguments);
+                cacheIn[key] = val;
+                return val;
+            }
+        }
+    },
+    draw(left,top,right,under,width,height,N) {
         this.ctx.clearRect(0, 0, width, height)
         this.ctx.font = "12px serif";
 
-        let lt = [-0.575,0.55];
-        let ru = [-0.570,0.54];
+        let lt = [left,top];
+        let ru = [right,under];
         if (ru[0]-lt[0]<0 || ru[1]-lt[1]>0 ){
             console.log("描画エラー");
             return
         }
 
-        for(var row=0;row<=height;row+=radius){
-            for(var col=0;col<=width;col+=radius){
+        for(var row=0;row<=height;row+=this.rad){
+            for(var col=0;col<=width;col+=this.rad){
                 //左上を(-2,2)、右下を(2,-2)の範囲で分割した数刻みで発散するかを計算
-                // let res = Math.random() > 0.5 ? 255 : 0 ;
-                let res = this.mandelbrot(0,0,
-                    lt[0]+col/width*(ru[0]-lt[0]),
-                    lt[1]+row/height*(ru[1]-lt[1])
-                    ,N
+                let precisionX = (lt[0]+col/width*(ru[0]-lt[0])).toPrecision(8);
+                let precisionY = (lt[1]+row/height*(ru[1]-lt[1])).toPrecision(8);
+
+                let factorial = this.memoize(this.mandelbrot);
+                let res = factorial(0,0,
+                    parseFloat(precisionX),parseFloat(precisionY),N
                 )
-                if(res != 255){
+                if(res < 2){
                     this.ctx.beginPath()
-                    this.ctx.arc(col, row, radius, 0, Math.PI * 2);
-                    let rgb =`rgb(${res}, ${res}, ${res})`;
+                    this.ctx.arc(col, row, this.rad, 0, Math.PI * 2);
+                    let gray = res/2*255
+                    let rgb =`rgb(${gray}, ${gray}, ${gray})`;
                     this.ctx.fillStyle = rgb;
                     this.ctx.fill();
                     this.ctx.closePath()
                 }
             }
         }
-        for(var row=0;row<=height;row+=radius){
-            for(var col=0;col<=width;col+=radius){
+        let middleHeight = parseInt(height/2);
+        let heightArray = [0,middleHeight,height]
+        let middleWidth = parseInt(width/2);
+        let widthArray = [0,middleWidth,width]
+        for (const row of heightArray) {
+            for (const col of widthArray) {
+                // console.log(`${row} ${col}`);
                 this.ctx.beginPath()
                 this.ctx.fillStyle = `rgb(255, 0, 0)`;
                 let x = lt[0]+col/width*(ru[0]-lt[0]);
                 let y = lt[1]+row/height*(ru[1]-lt[1]);
-                if ( col == 0 && row == 0 ){
-                    this.ctx.fillText(`lt(${x.toPrecision(2)},${y.toPrecision(2)})`,
-                    col+10, row+10);
-                }
-                if ( col == 0 && row == parseInt(height/2)){
-                    this.ctx.fillText(`l(${x.toPrecision(2)},${y.toPrecision(2)})`,
-                    col+10, row);
-                }
-                if ( col == 0 && row == height){
-                    this.ctx.fillText(`lu(${x.toPrecision(2)},${y.toPrecision(2)})`,
-                    col+10, row-10);
-                }
-                if ( col == parseInt(width/2) && row == 0){
-                    this.ctx.fillText(`t(${x.toPrecision(2)},${y.toPrecision(2)})`,
-                    col, row+10);
-                }
-                if ( col == parseInt(width/2) && row == parseInt(height/2)){
-                    this.ctx.fillText(`O(${x.toPrecision(2)},${y.toPrecision(2)})`,
-                    col, row);
-                }
-                if ( col == parseInt(width/2) && row == height){
-                    this.ctx.fillText(`u(${x.toPrecision(2)},${y.toPrecision(2)})`,
-                    col, row-10);
-                }
-                if ( col == width && row == 0){
-                    this.ctx.fillText(`rt(${x.toPrecision(2)},${y.toPrecision(2)})`,
-                    col-100, row+10);
-                }
-                if ( col == width && row == parseInt(height/2)){
-                    this.ctx.fillText(`r(${x.toPrecision(2)},${y.toPrecision(2)})`,
-                    col-100, row);
-                }
-                if ( col == width && row == height){
-                    this.ctx.fillText(`ru(${x.toPrecision(2)},${y.toPrecision(2)})`,
-                    col-100, row-10);
+                let atCol = (col == 0) ? 10: (col == width) ? -100 : 0;
+                let atRow = (row == 0) ? 10: (row == height) ? -10 : 0;
+                if ( ( col == 0 || col == parseInt(width/2) || col == width)
+                    && (row == 0 || row == parseInt(height/2) || row == height) ){
+                    this.ctx.fillText(`(${x.toPrecision(2)},${y.toPrecision(2)})`,
+                    col+atCol, row+atRow);
                 }
                 this.ctx.closePath()
             }
         }
     },
     mandelbrot(znr,zni,cr,ci,N){
+        let abs = 0;
         if (0>=N){
-            let abs = znr*znr+zni*zni;
-            return abs > 255 ? 255 : abs;
+            abs = znr*znr+zni*zni;
+            // console.log(`abs:${abs}`);
+            return abs >= 2 ? 2 : abs;
         }
         let zn1r = (znr * znr) - (zni * zni) + cr;
         let zn1i = (znr * zni * 2.0) + ci;
         if (!isFinite(zn1r)||!isFinite(zn1i)){
-            return 255;
+            return 2;
+        }
+        abs = znr*znr+zni*zni;
+        if (abs >= 2){
+            return 2;
         }
         return this.mandelbrot(zn1r,zn1i,cr,ci,N-1)
     },
@@ -120,7 +140,7 @@ export default {
   mounted() {
     this.ctx = this.$el.getContext('2d')
     console.log(`mounted height:${this.height} width:${this.width}`);
-    this.draw(this.radius,this.width,this.height,this.N)
+    this.draw(this.left,this.top,this.right,this.under,this.width,this.height,this.N)
   }
 }
 </script>
